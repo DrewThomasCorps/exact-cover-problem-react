875d925b55041e0053379bc074313e71cb276221
Drew Thomas <amthomas12@bsu.edu>
2020-03-29 18:10:34 -0400

    Animate the pentomino problem with speed controls

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ import PentominoRenderer from "./components/PentominoRenderer";
 function App() {
 
     let [pentomino, set_pentomino] = useState(null);
+    let [dancing_links, set_dancing_links] = useState(null);
+    let [interval_time, set_interval_time] = useState({current: 10, previous: 10});
     let [row_rendered, set_row_rendered] = useState(null);
 
     useEffect(() => {
         set_pentomino(new Pentomino());
     }, []);
+
+    useEffect(() => {
+        if (dancing_links !== null) {
+            const interval = setInterval(() => {
+                if (dancing_links.found_final_solutions) {
+                    dancing_links.found_final_solutions = false;
+                    set_interval_time({...interval_time, current: 1000});
+                } else {
+                    dancing_links.should_run = true;
+                    dancing_links.next_to_run();
+                    set_interval_time({...interval_time, current: interval_time.previous})
+                }
+            }, interval_time.current);
+            return () => clearInterval(interval);
+        }
+    }, [dancing_links, interval_time]);
 
 
     useEffect(() => {
@@ -20,14 +38,20 @@ function App() {
             return;
         }
         let matrix = new Matrix(pentomino.exact_cover_matrix);
-        let links =new DancingLinks(matrix, set_row_rendered);
-        set_row_rendered(links.final_solutions[2]);
+        set_dancing_links(new DancingLinks(matrix, set_row_rendered));
     }, [pentomino]);
 
     return (
         row_rendered &&
         <div className="App">
             <PentominoRenderer board={pentomino.board} solutions_row={row_rendered}/>
+            <input value={interval_time.current} onChange={(e) => {
+                let value = parseInt(e.target.value);
+                if (isNaN(value)){
+                    value = 0;
+                }
+                set_interval_time({current: value, previous: value})
+            }}/>
         </div>
     );
 }
--- src/utilities/DancingLinks.js
+++ src/utilities/DancingLinks.js
@@ -1,6 +1,9 @@
 class DancingLinks {
     matrix;
     set_row_rendered;
+    should_run = true;
+    next_to_run;
+    found_final_solutions = false;
     final_solutions = [];
     current_solution = [];
 
@@ -8,17 +11,13 @@ class DancingLinks {
         this.matrix = matrix;
         this.set_row_rendered = set_row_rendered;
         this.get_exact_cover_solutions();
-        this.get_next_solution();
     }
-
-    get_next_solution = () => {
-        while (this.backtrack()) {
-            this.get_exact_cover_solutions();
-        }
-    };
 
     get_exact_cover_solutions = () => {
         while (this.matrix.columns.head.right !== this.matrix.columns.head) {
+            if (!this.should_run) {
+                return;
+            }
             let chosen_column = this.matrix.get_column_with_fewest_nodes();
             if (chosen_column.total_nodes === 0) {
                 if(this.backtrack()){
@@ -30,31 +29,42 @@ class DancingLinks {
             let node = chosen_column.down;
             this.matrix.cover(node);
             this.current_solution.push(node);
+            this.set_row_rendered([...this.current_solution]);
+            this.should_run = false;
+            this.next_to_run = this.get_exact_cover_solutions;
         }
         this.final_solutions.push([...this.current_solution]);
+        this.found_final_solutions = true;
+        this.next_to_run = this.backtrack;
     };
 
     backtrack = () => {
-        let last_node = this.uncover_current_node();
-        while (last_node && last_node.down === last_node.head) {
-            last_node = this.uncover_current_node();
-        }
-        if (!last_node) {
-            return false;
-        }
-        let node = last_node.down;
-        this.matrix.cover(node);
-        this.current_solution.push(node);
-        return true
+        return this.uncover_current_node();
     };
 
     uncover_current_node = () => {
         let last_node = this.current_solution.pop();
         if (last_node === undefined) {
+            this.next_to_run = () => {};
             return false;
         }
         this.matrix.uncover(last_node);
+        this.set_row_rendered([...this.current_solution]);
+        this.should_run = false;
+        if (last_node.down === last_node.head) {
+            this.next_to_run = this.uncover_current_node;
+        } else {
+            this.next_to_run = () => {this.cover_node(last_node.down)}
+        }
         return last_node;
+    };
+
+    cover_node = (node) => {
+        this.matrix.cover(node);
+        this.current_solution.push(node);
+        this.set_row_rendered([...this.current_solution]);
+        this.should_run = false;
+        this.next_to_run = this.get_exact_cover_solutions;
     }
 }
 
